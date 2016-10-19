c685010258cf6fcca2e2be93e3df025fd02f1594
ajbc <ajbchaney@gmail.com>
2016-10-19 15:56:41 -0400

    faster script

--- misc/make_vw_data.py
+++ misc/make_vw_data.py
@@ -1,35 +1,4 @@
-ids = ''
-fout = open("data.tsv", 'w+')
-line_id = 0
-for line in open("data_inclmissing.tsv"):
-    line_id += 1
-    if ids=='':
-        ids = line.strip('\n').split('\t')
-        continue
-    tok = line.strip('\n').split('\t')
-    id = int(tok[0].strip("\""))
-    idx = 1
-    if len(ids) != len(tok):
-        print line_id
-        print len(ids), len(tok)
-    for t in tok[2:]:
-        idx += 1
-        if t == 'NA':
-            continue
-        t = t.strip("\"")
-        if '-' in t:
-            continue
-        #if float(t.strip("\"")) < 0:
-        #    continue
-        fout.write("%d\t%s\t%s\n" % (id, ids[idx].strip("\""), t))
-fout.close()
-
-fout_gpa = open("vw_gpa.dat", 'w+')
-fout_grit = open("vw_grit.dat", 'w+')
-fout_attc = open("vw_attachment.dat", 'w+')
-fout_evic = open("vw_evicted.dat", 'w+')
-fout_colp = open("vw_collegeplans.dat", 'w+')
-
+print "reading in training outcomes"
 ids = ''
 cur = None
 cur_str = ''
@@ -44,49 +13,67 @@ for line in open("train_outcomes.csv"):
         1 if int(tok[4]) == 1 else -1, \
         1 if int(tok[5]) == 1 else -1)
 
+
+print "setting up vw files"
+fout_gpa = open("vw_gpa.dat", 'w+')
+fout_grit = open("vw_grit.dat", 'w+')
+fout_attc = open("vw_attachment.dat", 'w+')
+fout_evic = open("vw_evicted.dat", 'w+')
+fout_colp = open("vw_collegeplans.dat", 'w+')
+fout2_gpa = open("vw_gpa_test.dat", 'w+')
+fout2_grit = open("vw_grit_test.dat", 'w+')
+fout2_attc = open("vw_attachment_test.dat", 'w+')
+fout2_evic = open("vw_evicted_test.dat", 'w+')
+fout2_colp = open("vw_collegeplans_test.dat", 'w+')
 def write_out(idx, features):
-    if idx not in outcomes:
-        return
     f = "|MetricFeatures%s" % features[0]
     if len(features) > 1:
         for i in range(1,len(features)):
             f += ' ' + features[i]
     f += '\n'
+    
+    if idx not in outcomes:
+        fout2_gpa.write("0 %s" % f)
+        fout2_grit.write("0 %s" % f)
+        fout2_attc.write("0 %s" % f)
+        fout2_evic.write("0 %s" % f)
+        fout2_colp.write("0 %s" % f)
 
-    fout_gpa.write("%f %s" % (outcomes[idx][0], f))
-    fout_grit.write("%f %s" % (outcomes[idx][1], f))
-    fout_attc.write("%f %s" % (outcomes[idx][2], f))
-    fout_evic.write("%d %s" % (outcomes[idx][3], f))
-    fout_colp.write("%d %s" % (outcomes[idx][4], f))
-    
-    
+    else:
+        fout_gpa.write("%f %s" % (outcomes[idx][0], f))
+        fout_grit.write("%f %s" % (outcomes[idx][1], f))
+        fout_attc.write("%f %s" % (outcomes[idx][2], f))
+        fout_evic.write("%d %s" % (outcomes[idx][3], f))
+        fout_colp.write("%d %s" % (outcomes[idx][4], f))
+        
+
+print "processing training data"
 ids = ''
-cur = None
-cur_str = ['']
-lc = 0
-for line in open("data.tsv"):
-    lc +=1
-    #if lc > 10000:
-    #    break
-    if ids == '':
-        ids = line.strip().split(',')
+for line in open("data_inclmissing.tsv"):
+    if ids=='':
+        ids = line.strip('\n').split('\t')
         continue
-    
     tok = line.strip('\n').split('\t')
-    idx = int(tok[0])
+    id = int(tok[0].strip("\""))
+    print "child", id
+    idx = 1
+    cur_str = ['']
+    for t in tok[2:]:
+        idx += 1
 
-    if idx != cur:
-        if cur_str != ['']:
-            print cur
-            write_out(cur, cur_str)
-        cur = idx
-        cur_str = ['']
+        # remove na and negative-valued covariates
+        if t == 'NA':
+            continue
+        t = t.strip("\"")
+        if '-' in t:
+            continue
+        
+        # add the covariat in
+        try:
+            val = float(t)
+            cur_str[0] += ' ' + ids[idx].strip("\"") + ":" + t
+        except:
+            if tok[2] != '':
+                cur_str.append('|%s %s' % (ids[idx].strip("\""), t))
 
-    try:
-        val = float(tok[2])
-        cur_str[0] += ' ' + tok[1] + ":" + tok[2]
-    except:
-        if tok[2] != '':
-            cur_str.append('|%s %s' % (tok[1], tok[2]))
-
-write_out(cur, cur_str)
+    write_out(id, cur_str)
